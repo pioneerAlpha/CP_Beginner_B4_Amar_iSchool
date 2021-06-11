e2f14c282094d3fe1ecb46ae070c05790ea0e38a
tbssajal <45002968+tbssajal@users.noreply.github.com>
2021-06-12 01:01:25 +0600

    Create binary search.cpp

---
+++ class 15 - binary search/binary search.cpp
@@ -0,0 +1,85 @@
+//#include<cstdio>
+//#include<cassert>
+//#include<iostream>
+//#include<cstring>
+//#include<algorithm>
+#include<bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+#define MAX ((int)2e9 + 5)
+#define MAXP ((int)1e5 + 5)
+#define MAXL ((ll)1e18 + 5)
+#define MAX_X ((int)2001)
+#define MAX_Y ((int)2001)
+#define pi acos(-1)
+#define MOD ((int)1e9 + 7)
+//#define MOD ((int)998244353 + 0)
+#define BAS ((int)1e6 + 3)
+//#define BAS ((int)2e5 + 3)
+#define N ((int)1e5 + 9)
+#define eps (1e-8)
+#define fastio ios_base::sync_with_stdio(false),cin.tie(NULL)
+#define logn 17
+#define endl "\n"
+#define mpp make_pair
+#define BUCK 105
+#define LEF (idx<<1)
+#define RIG ((idx<<1)|1)
+//#define int ll
+
+
+using namespace std;
+using namespace __gnu_pbds;
+
+typedef long long ll;
+typedef unsigned long long ull;
+
+/*fast io
+ios_base::sync_with_stdio(false);
+cin.tie(NULL);
+*/
+
+
+
+
+typedef tree < int,  null_type,  less < int >,  rb_tree_tag,  tree_order_statistics_node_update > o_set;
+typedef tree < pair < int, int >,  null_type,  less < pair < int, int > >,  rb_tree_tag,  tree_order_statistics_node_update > o_setp;
+/// o_set s;
+/// s.order_of_key(k) : Number of items strictly smaller than k .
+/// *(s.find_by_order(k)) : K-th element in a set (counting from zero).
+
+typedef long double ldd;
+
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+int my_rand(int l, int r)
+{
+    return uniform_int_distribution<int>(l,r) (rng);
+}
+
+
+
+
+int main()
+{
+    /// binary search on sorted array
+    int n;
+    cin>>n;
+    for(int i = 1 ; i <= n ; i++) cin>>arr[i];
+    int val;
+    cin>>val;
+    int L = 1 , R = n;
+    while(L < R){
+        int mid = (L+R)/2;
+//        if(arr[mid] == val){
+//            cout<<mid<<endl;
+//            return 0;
+//        }
+        if(arr[mid] < val) L = mid+1;
+        else R = mid; /// arr[mid] >= val;
+        /// L = 2 , R = 3 , mid = 2
+    }
+    if(arr[L] == val) cout<<L<<endl;
+    else cout<<"-1\n";
+    return 0;
+}
