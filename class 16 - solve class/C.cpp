1d6d72d83e1f6c62e8c5b417920c4e50b078b873
tbssajal <45002968+tbssajal@users.noreply.github.com>
2021-06-18 02:11:35 +0600

    Create C.cpp

---
+++ class 16 - solve class/C.cpp
@@ -0,0 +1,93 @@
+//#include<cstdio>
+//#include<cassert>
+//#include<iostream>
+//#include<cstring>
+//#include<algorithm>
+#include<bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+#define MAX ((int)2e9 + 5)
+#define MAXP ((int)1e5 + 5)
+#define MAXL ((ll)1e18 + 5)
+#define MAX_X ((int)2001)
+#define MAX_Y ((int)2001)
+#define pi acos(-1)
+#define MOD ((int)1e9 + 7)
+//#define MOD ((int)998244353 + 0)
+#define BAS ((int)1e6 + 3)
+//#define BAS ((int)2e5 + 3)
+#define N ((int)1e5 + 9)
+#define eps (1e-8)
+#define fastio ios_base::sync_with_stdio(false),cin.tie(NULL)
+#define logn 17
+#define endl "\n"
+#define mpp make_pair
+#define BUCK 105
+#define LEF (idx<<1)
+#define RIG ((idx<<1)|1)
+//#define int ll
+//#pragma GCC optimize("Ofast,unroll-loops")
+//#pragma GCC target("avx,avx2,fma")
+
+using namespace std;
+using namespace __gnu_pbds;
+
+typedef long long ll;
+typedef unsigned long long ull;
+
+/*fast io
+ios_base::sync_with_stdio(false);
+cin.tie(NULL);
+*/
+
+
+
+
+typedef tree < int,  null_type,  less < int >,  rb_tree_tag,  tree_order_statistics_node_update > o_set;
+typedef tree < pair < int, int >,  null_type,  less < pair < int, int > >,  rb_tree_tag,  tree_order_statistics_node_update > o_setp;
+/// o_set s;
+/// s.order_of_key(k) : Number of items strictly smaller than k .
+/// *(s.find_by_order(k)) : K-th element in a set (counting from zero).
+
+typedef long double ldd;
+
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+int my_rand(int l, int r)
+{
+    return uniform_int_distribution<int>(l,r) (rng);
+}
+
+
+int arr[N];
+
+int srch(int val , int L , int R) /// returns the index of the 1st element
+{                           /// from left which is greater than or equal to val
+    while(L<R){
+        int mid = (L+R)>>1;
+        if(arr[mid] >= val) R = mid;
+        else L = mid+1;
+    }
+    return L;
+}
+
+int main()
+{
+    fastio;
+    int t ,caseno = 1;
+    cin>>t;
+    while(t--){
+        int n , q;
+        cin>>n>>q;
+        for(int i = 1 ; i<=n ; i++) cin>>arr[i];
+        sort(arr+1 , arr+n+1);
+        arr[n+1] = MAX;
+        cout<<"Case "<<caseno++<<":\n";
+        while(q--){
+            int a , b;
+            cin>>a>>b;
+            cout<<srch(b+1,1,n+1) - srch(a,1,n+1)<<endl;
+        }
+    }
+    return 0;
+}
